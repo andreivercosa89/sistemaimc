86407ceda36133bd8b97745a619c8d5448692cbe
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-10-31 21:27:34 -0400

    calcular IMC

---
+++ sistemaimc/Imc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace sistemaimc
+{
+    public class Imc
+    {
+        public string Nome {set;get;}
+
+        public double Peso {set; get;}
+
+        public double Altura {set; get;}
+
+        public double testImc(){
+            return (Peso/Math.Pow(Altura,2));
+        }
+
+        public string getImc(){
+           if(testImc() < 18.5)
+            {
+                return  "Abaixo";
+            }else if (testImc() < 25){
+                return "Normal";
+            }else if (testImc() < 35)
+            {
+                return "Ob. Leve";
+            }else if (testImc() < 40)
+            {
+                return "Ob. Moderado";
+            }else{
+                return "Ob. Morbido";
+            }
+        }
+    }
+}
---
+++ sistemaimc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sistemaimc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 1;
+
+            do
+            {
+
+                Imc imc = new Imc();
+
+                Console.WriteLine("Informe o Nome do aluno: ");
+                imc.Nome = Console.ReadLine().ToUpper();
+
+                Console.WriteLine("Informe o Peso: ");
+                imc.Peso = Double.Parse(Console.ReadLine().Replace(".",","));
+
+                Console.WriteLine("Informe a Altura: ");
+                imc.Altura = Double.Parse(Console.ReadLine().Replace(".", ","));
+
+                Console.WriteLine("{0}: {1}", imc.Nome, imc.getImc());
+
+                Console.WriteLine("\n\nDeseja continuar: \n1 - Sim\n2 - Nao");
+                op = Int32.Parse(Console.ReadLine());
+                
+            } while (op == 1);
+        }
+    }
+}
